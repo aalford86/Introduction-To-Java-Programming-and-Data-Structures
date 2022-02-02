2b777830a9f93121251451eb03b6fc88ec2cf37a
Alex Alford <98012528+aalford86@users.noreply.github.com>
2022-02-01 22:08:54 -0500

    Add Exercise 1.1 Solution
    Exercise 1.1 - Display Three Messages added to repository.

---
+++ src/chapter_01/Exercise_1_01.java
@@ -0,0 +1,48 @@
+package chapter_01;
+
+/**
+ * Exercise 1.1
+ *
+ * (Display Three Messages) Write a program that displays "Welcome to Java",
+ * "Welcome to Computer Science", and "Programming is fun".
+ *
+ * @Book Introduction to JAVA Programming, 11 Ed. By Daniel Liang
+ * @Date February 2022
+ * @author Alex Alford
+ *
+ */
+public final class Exercise_1_01 {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private Exercise_1_01() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /*
+         * Program Title.
+         */
+        System.out.println("Exercise 1.1: Display Three Messages");
+
+        /*
+         * Print three messages to console.
+         */
+        System.out.println("\nWelcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+
+        /*
+         * Close output stream.
+         */
+        System.out.close();
+    }
+
+}
