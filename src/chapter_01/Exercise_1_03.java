efc6de668f833b7534d9893e795b3c90a5db43de
Alex Alford <98012528+aalford86@users.noreply.github.com>
2022-02-01 22:29:50 -0500

    Add Exercise *1.3 Solution
    Exercise *1.3 - Display A Pattern added to repository.

---
+++ src/chapter_01/Exercise_1_03.java
@@ -0,0 +1,50 @@
+package chapter_01;
+
+/**
+ * Exercise *1.3
+ *
+ * (Display a pattern) Write a program that displays the following pattern:
+ * "JAVA".
+ *
+ * @Book Introduction to JAVA Programming, 11 Ed. By Daniel Liang
+ * @Date February 2022
+ * @author Alex Alford
+ *
+ */
+public final class Exercise_1_03 {
+
+    /**
+     * Default constructor--private to prevent instantiation.
+     */
+    private Exercise_1_03() {
+
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments; unused here
+     */
+    public static void main(String[] args) {
+
+        /*
+         * Program Title.
+         */
+        System.out.println("Exercise *1.3: Display a Pattern\n");
+
+        /*
+         * Output "JAVA" pattern to console.
+         */
+        System.out.println("   J    A   V     V   A   ");
+        System.out.println("   J   A A   V   V   A A  ");
+        System.out.println("J  J  AAAAA   V V   AAAAA ");
+        System.out.println(" JJ  A     A   V   A     A");
+
+        /*
+         * Close console output stream.
+         */
+        System.out.close();
+    }
+
+}
