b178b52a46fb2a7c2dec53e4e1894be19146774a
Alex Alford <98012528+aalford86@users.noreply.github.com>
2022-02-01 23:53:30 -0500

    Add Exercise 1.13 Solution
    Exercise 1.13 - Algebra Solve 2x2 Linear Equations added to repository.

---
+++ src/chapter_01/Exercise_1_13.java
@@ -0,0 +1,64 @@
+package chapter_01;
+
+/**
+ * Exercise *1.13
+ *
+ * (Algebra: Solve 2 X 2 linear equations) You can use Cramer's rule to solve
+ * the following 2 X 2 system of linear equation provided that ad - bc is not 0:
+ *
+ * <pre>
+ *
+ * Formula
+ *
+ * ax + by = e       ed - bf         af - ec
+ *              x = ---------   y = ---------
+ * cx + dy = f       ad - bc         ad - bc
+ *
+ * </pre>
+ *
+ * Write a program that solves the following equation and displays the value of
+ * x and y: (Hint: Replace the symbols in the formul with numbers to compute x
+ * and y. This exercise can be done in Chapter 1 without using materials in
+ * later chapters.)
+ *
+ * <pre>
+ *
+ * 3.4x + 50.2y = 44.5
+ * 2.1x + 0.55y = 5.9
+ * </pre>
+ *
+ *
+ * @Book Introduction to JAVA Programming, 11 Ed. By Daniel Liang
+ * @Date February 2022
+ * @author Alex Alford
+ *
+ */
+public final class Exercise_1_13 {
+
+	/**
+	 * Default constructor--private to prevent instantiation.
+	 */
+	private Exercise_1_13() {
+
+	}
+
+	/**
+	 * Main method.
+	 *
+	 * @param args the command line arguments; unused here
+	 */
+	public static void main(String[] args) {
+
+		/*
+		 * Program Title.
+		 */
+		System.out.println("Exercise *1.13: Algebra Solve 2x2 Linear Equations\n");
+
+		/*
+		 * Output message to console.
+		 */
+		System.out.println("x = " + ((44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1)));
+		System.out.println("y = " + ((3.4 * 5.9 - 44.5 * 2.1) / (3.4 * 0.55 - 50.2 * 2.1)));
+	}
+
+}
