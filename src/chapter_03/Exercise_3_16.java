5ca9f1176f21ace7756ea00b5c524730987142c0
Alex Alford <98012528+aalford86@users.noreply.github.com>
2022-02-03 03:35:14 -0500

    Add Exercise 3.16 Solution
    Exercise 3.16 - Random Point added to repository.

--- src/chapter_03/Exercise_3_16.java
+++ src/chapter_03/Exercise_3_16.java
@@ -3,9 +3,10 @@ package chapter_03;
 import java.util.Scanner;
 
 /**
- * Exercise
+ * Exercise 3.16
  *
- *
+ * (Random Point) Write a program that displays a random coordinate in a
+ * rectangle. The rectangle is centered at (0, 0) with width 100 and height 200.
  *
  * @Book Introduction to JAVA Programming, 11 Ed. By Daniel Liang
  * @Date February 2022
@@ -31,12 +32,38 @@ public final class Exercise_3_16 {
 		/*
 		 * Program Title.
 		 */
-		System.out.println("Exercise ");
+		System.out.println("Exercise 3.16 - Random Point");
 
 		/*
 		 * Open input stream.
 		 */
 		Scanner consoleInput = new Scanner(System.in);
+
+		/*
+		 * Declare the constants for the maximum and minimum values that the x and y
+		 * coordinates can be.
+		 */
+		final double X_COORD_MAX = 50.0;
+		final double X_COORD_MIN = -50.0;
+		final double Y_COORD_MAX = 100.0;
+		final double Y_COORD_MIN = -100.0;
+
+		/*
+		 * Generate a random x coordinate with in a set range of possible coordinates.
+		 */
+		double xCoordinate = Math.random() * ((X_COORD_MAX - X_COORD_MIN) + 1) + X_COORD_MIN;
+
+		/*
+		 * Generate a random y coordinate with in a set range of possible coordinates.
+		 */
+		double yCoordinate = Math.random() * ((Y_COORD_MAX - Y_COORD_MIN) + 1) + Y_COORD_MIN;
+
+		/*
+		 * Output result to console.
+		 */
+		System.out.println("");
+		System.out.println("The random x coordinate: " + xCoordinate);
+		System.out.println("The random y coordinate: " + yCoordinate);
 
 		/*
 		 * Close input stream.
