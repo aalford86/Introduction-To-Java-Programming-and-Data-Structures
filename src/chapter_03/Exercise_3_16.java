7913cf6852a49d04ef7638d01c2de697d47f15ed
Alex Alford <98012528+aalford86@users.noreply.github.com>
2022-02-03 03:26:19 -0500

    Add Exercise 3.16 Solution
    Exercise 3.16 - Random Point added to repository.

---
+++ src/chapter_03/Exercise_3_16.java
@@ -0,0 +1,47 @@
+package chapter_03;
+
+import java.util.Scanner;
+
+/**
+ * Exercise
+ *
+ *
+ *
+ * @Book Introduction to JAVA Programming, 11 Ed. By Daniel Liang
+ * @Date February 2022
+ * @author Alex Alford
+ *
+ */
+public final class Exercise_3_16 {
+
+	/**
+	 * Private constructor so this utility class cannot be instantiated.
+	 */
+	private Exercise_3_16() {
+
+	}
+
+	/**
+	 * Main method.
+	 *
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+
+		/*
+		 * Program Title.
+		 */
+		System.out.println("Exercise ");
+
+		/*
+		 * Open input stream.
+		 */
+		Scanner consoleInput = new Scanner(System.in);
+
+		/*
+		 * Close input stream.
+		 */
+		consoleInput.close();
+	}
+
+}
